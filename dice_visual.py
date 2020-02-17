0ef5700e8fe343b56b13a85ff2794780ecadb68b
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-17 20:50:01 +0100

    Updated code.

--- dice_visual.py
+++ dice_visual.py
@@ -8,13 +8,14 @@ die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-times = 10_000
+times = 50_000
 for time in range(1, times+1):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze results.
 results_range = sorted(set(results))
+print(set(results))
 frequencies = []
 for result in results_range:
     counts = results.count(result)
@@ -26,6 +27,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 10.000 times',
+my_layout = Layout(title='Results of rolling twp d6 dice 50.000 times',
     xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
